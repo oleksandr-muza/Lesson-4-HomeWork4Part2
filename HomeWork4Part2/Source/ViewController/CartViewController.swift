d0735bde200f645a7247c0ecb77fec50f697d4b5
oleksandr-muza <131881080+oleksandr-muza@users.noreply.github.com>
2024-03-03 23:17:45 +0200

    bug 4 fixed

--- HomeWork4Part2/Source/ViewController/CartViewController.swift
+++ HomeWork4Part2/Source/ViewController/CartViewController.swift
@@ -49,6 +49,17 @@ class CartViewController: UIViewController {
     }
     
     @IBAction func discountSegmentAction(_ segmentControl: UISegmentedControl) {
+        let selectedIndex = segmentControl.selectedSegmentIndex
+        switch selectedIndex {
+        case 0:
+            discount = .none
+        case 1:
+            discount = .regular
+        case 2:
+            discount = .vip
+        default:
+            break
+        }
         showChanges()
     }
     
